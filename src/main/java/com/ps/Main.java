b249a7f1fdc7b8624aebaed97507ec48639a5f49
Thierno <podiallo2242@gmail.com>
2025-04-12 00:03:30 -0400

    Added user choice prompts

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,8 +1,19 @@
 package com.ps;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+       Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to Financial Calculator App!");
+        System.out.println("Please select a calculator:");
+        System.out.println("1 - Mortgage Calculator");
+        System.out.println("2 - Future Value Calculator");
+        System.out.println("3 - Present Value of Annuity Calculator");
+
+        System.out.print("Enter your choice (1, 2 or 3): ");
+        int choice = scanner.nextInt();
+
+
+        scanner.close();
     }
 }
